0d1a2cff6925063609f4727d3480b08e037f9f6f
Johnny <johnnyduff25@gmail.com>
2024-12-30 13:20:54 -0300

    adicionado método create para a task, criado classe de Task

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from models.task import Task
 app = Flask(__name__)
 
-
-@app.route("/")
-def home():
-    return "Página Home"
+tasks = []
+task_id_control = 1
 
 
-@app.route("/about")
-def about():
-    return "Essa é a pagina sobre"
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control, title=data.get(
+        'title'), description=data.get('description', ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    return jsonify({"message": 'Nova tarefa criada com sucesso!'})
 
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
